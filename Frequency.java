14c8dd352f985c221e8e609c8217a9fa643eaa88
Spencer J. Ewall <sewall@wesleyan.edu>
2011-11-03 03:58:05 -0400

    Removed unnecessary comments from Frequency.java

--- Frequency.java
+++ Frequency.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of class FrequencyPlot here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Frequency
 {
     int hSegments;
