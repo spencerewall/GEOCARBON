8468af639537cc045dd5f301a2f9e20110a1d2b9
Spencer J. Ewall <sewall@wesleyan.edu>
2011-11-05 19:10:02 -0400

    Changed base of data holders from abstract CO2Run.java to HistData.java interface

--- HistData.java
+++ HistData.java
@@ -2,7 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 
-public interface CO2Run
+/**
+ * __________ is 
+ */
+public interface HistData
 {
     public float getCO2(int i);
     public float[] getAllCO2();
