97f1a7254078201dac16791c1c436b497048d59e
Spencer J. Ewall <sewall@wesleyan.edu>
2011-11-06 15:48:19 -0500

    CommonData class created.
    This class stores a data run for which each point is taken from
    different specific HistData objects

---
+++ CommonData.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+
+public class CommonData implements HistData
+{
+    ArrayList<HistData> data;
+    
+    public CommonData()
+    {
+        data = new ArrayList();
+    }
+    public CommonData(int len)
+    {
+        data = new ArrayList(len);
+    }
+    public void nextPointFrom(HistData d)
+    {
+        data.add(d);
+    }
+    public float getCO2(int i)
+    {
+        return data.get(i).getCO2(i);
+    }
+    public float[] getAllCO2()
+    {
+        float[] returnMe = new float[data.get(0).size()];
+        int c = 0;
+        while ( c<returnMe.length )
+        {
+            HistData thisRun = data.get(c);
+            
+        }
+        return returnMe;
+    }
+    public int size()
+    {
+        return data.size();
+    }
+    public boolean equals(Object other)
+    {
+        if (other instanceof HistData)
+        {
+            HistData o = (HistData) other;
+            if(size() == o.size())
+            {
+                
+            }
+        }
+        return false;
+    }
+    public int hashCode()
+    {
+        return data.hashCode();
+    }
+    
+    
+    
+    
+    
+    
+    public class PPMRunPair implements Comparable{
+        private double pp; private HistData rn;
+        public PPMRunPair(double ppm, HistData r) {
+            pp=ppm; rn=r;
+        }
+        public int compareTo(Object other) throws ClassCastException{
+            if (this == other)
+                return 0;
+            if (!(other instanceof PPMRunPair))
+                throw new ClassCastException("PPMRunPair Object was expected.");
+            double otherPP = ((PPMRunPair)other).getPPM();
+            return (int) Math.signum(pp-otherPP);
+        }
+        public boolean equals(Object other) {
+            if (this==other)
+                return true;
+            if (!(other instanceof PPMRunPair))
+                return false;
+            PPMRunPair o = (PPMRunPair) other;
+            return ((pp==o.getPPM()));
+            
+        }
+        public double getPPM() {   
+            return pp; 
+        }
+        public HistData getRun() {
+            return rn; 
+        }
+    }
+}
