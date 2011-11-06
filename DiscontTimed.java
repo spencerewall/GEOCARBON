aea210eb6574101dbe3704d96b73d958fb74a5ed
Spencer J. Ewall <sewall@wesleyan.edu>
2011-11-06 16:02:29 -0500

    Interface added for inclusion of timescales

---
+++ DiscontTimed.java
@@ -0,0 +1,10 @@
+
+/**
+ * Abstract class DiscontTimed - write a description of the class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public abstract class DiscontTimed
+{
+}
