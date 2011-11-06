2431f6280e5c81a70542939c069e4d5c648475df
Spencer J. Ewall <sewall@wesleyan.edu>
2011-11-06 15:46:16 -0500

    Plot methods updated

--- MeanPlot.R
+++ MeanPlot.R
@@ -1,2 +1,13 @@
+library(MASS)
+
 out <- read.table("output.dat")
-plot(outoutout$V1, outoutout$V2 , ylim=c(0,2000), xlim=c(0, -600))
+kde2d(out)
+
+library(KernSmooth)
+fit = bkde2D(out$V1, out$V2)
+persp(fit$x1, fit$x2, fit$fhat)
+library(rgl)
+
+
+myCols <- colorRampPalette(c("white", "green", "red"))
+smoothScatter(out$V1, out$V2 , ylim=c(0,3000), xlim=c(0, -300), colramp = myCols)
