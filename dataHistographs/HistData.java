fa78bfa1c80eca69a58b26fdcb5a77c8fcd9bbab
Spencer J. Ewall <sewall@wesleyan.edu>
2011-11-17 09:44:18 -0500

    dataHistographs package created
    This package is intended to contain our CO2 data holders.  HistData
    moved to this package

---
+++ dataHistographs/HistData.java
@@ -0,0 +1,12 @@
+package dataHistographs;
+/**
+ * __________ is 
+ */
+public interface HistData
+{
+    public float getCO2(int i);
+    public float[] getAllCO2();
+    public int size();
+    public boolean equals(Object other);
+    public int hashCode();
+}
