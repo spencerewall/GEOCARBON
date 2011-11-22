3819ec083a7c47b482a364f8e36995a0b73aea5c
Spencer J. Ewall <sewall@wesleyan.edu>
2011-11-22 08:49:41 -0500

    GEOCARB3.java now includes original prokoph and worsley tests

--- GEOCARB3.java
+++ GEOCARB3.java
@@ -28,6 +28,60 @@ public class GEOCARB3
         double[] glac = new double[10000];
         Arrays.fill(glac, 1.0);
         
+        prokophTest(act, fert, life, gym, glac);
+    }
+    public static RunList performTests(int numTests, double[] ACT, double[] FERT, double[] LIFE, double[] GYM, double[] GLAC, GCSVData arrIn)
+    {
+        int ndel=28;
+        double deltaT=.28;
+        RunList tempList = new RunList();
+        for (int iiit=0; iiit<ndel; iiit++)
+        {
+            boolean b;
+            deltaT = deltaT+0.12*(1.0+4.0*((float) (iiit))/((float) (ndel-1)));
+            for (int c=0; c<10000; c++)
+            {
+                b=tempList.add(new FactorData(deltaT, ACT[c], FERT[c], LIFE[c], GYM[c], GLAC[c], arrIn));
+                if (!b)
+                    System.out.println("why won't you be added goddamn you?!");
+            } 
+            System.out.println("done"+iiit+"\tTime = "+System.currentTimeMillis());
+        }
+        return tempList;
+    }
+    public static void prokophTest(double[] act, double[] fert, double[] life, double[] gym, double[] glac)
+    {
+        double[] percentiles = {.025,.975, .5};
+        
+        RunList defaultRuns = performTests(10000, act, fert, life, gym, glac, new GCSVData());
+        
+        
+        System.out.println("Default Runs finished calculating.");
+        ArrayList<HistData> aList = new ArrayList();
+        aList.add(defaultRuns.getMean());
+        CommonData[] acom = defaultRuns.selectManyPercentileRuns(percentiles);
+        aList.add(acom[0]);
+        aList.add(acom[1]);
+        aList.add(acom[2]);
+        System.out.println("Now writing default runs.");
+        Util.writeRuns("prokophDefault.dat", aList);
+        
+        System.out.println("Whew, writing all that was sure tough, I think I could use a break");
+        GCSVData prokophData = new GCSVData();
+        prokophData.setDLC0(GCSVData.DLC0_PROKOPH);
+        RunList prokoph = performTests(10000, act, fert, life, gym, glac, prokophData);
+        System.out.println("Alternative tests finished calculating.");
+        ArrayList<HistData> bList = new ArrayList();
+        bList.add(prokoph.getMean());
+        CommonData[] bcom = prokoph.selectManyPercentileRuns(percentiles);
+        bList.add(bcom[0]);
+        bList.add(bcom[1]);
+        bList.add(bcom[2]);
+        System.out.println("Begin write.");
+        Util.writeRuns("prokophAltDLC0.dat",bList);
+    }
+    public static void worsleyTest(double[] act, double[] fert, double[] life, double[] gym, double[] glac)
+    {
         double[] percentiles = {.025,.975, .5};
         
         RunList defaultRuns = performTests(10000, act, fert, life, gym, glac, new GCSVData());
@@ -55,8 +109,6 @@ public class GEOCARB3
         System.out.println("Begin write.");
         Util.writeRuns("worsleyAltFA0.dat",bList);
         
-        
-        
         System.out.println("Whew, writing all that was sure tough, I think I could use a break");
         GCSVData worsleyPostData = new GCSVData();
         worsleyPostData.setFA0(GCSVData.fA0_WORSLEYPOST);
@@ -70,24 +122,5 @@ public class GEOCARB3
         cList.add(ccom[2]);
         System.out.println("Begin write.");
         Util.writeRuns("worsleyPostFA0.dat",cList);
-    }
-    public static RunList performTests(int numTests, double[] ACT, double[] FERT, double[] LIFE, double[] GYM, double[] GLAC, GCSVData arrIn)
-    {
-        int ndel=28;
-        double deltaT=.28;
-        RunList tempList = new RunList();
-        for (int iiit=0; iiit<ndel; iiit++)
-        {
-            boolean b;
-            deltaT = deltaT+0.12*(1.0+4.0*((float) (iiit))/((float) (ndel-1)));
-            for (int c=0; c<10000; c++)
-            {
-                b=tempList.add(new FactorData(deltaT, ACT[c], FERT[c], LIFE[c], GYM[c], GLAC[c], arrIn));
-                if (!b)
-                    System.out.println("why won't you be added goddamn you?!");
-            } 
-            System.out.println("done"+iiit+"\tTime = "+System.currentTimeMillis());
-        }
-        return tempList;
     }
 }
